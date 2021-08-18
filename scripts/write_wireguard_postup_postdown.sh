3bfed79753499477c39522f76a40dfb705b06c64
jsloane <186535+jsloane@users.noreply.github.com>
2021-08-18 22:28:33 +1000

    Update write_wireguard_postup_postdown.sh

--- scripts/write_wireguard_postup_postdown.sh
+++ scripts/write_wireguard_postup_postdown.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+cp /etc/wireguard/template-post-up.sh /etc/wireguard/post-up.sh
+cp /etc/wireguard/template-post-down.sh /etc/wireguard/post-down.sh
+
+sed -i "s#internet_ip#$1#" /etc/wireguard/post-up.sh
+sed -i "s#internet_ip#$1#" /etc/wireguard/post-down.sh
+
+chmod ugo+x /etc/wireguard/post-up.sh
+chmod ugo+x /etc/wireguard/post-down.sh
